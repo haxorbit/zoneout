c70103273f28ceae7532cee13bf9341db47cd558
haxorbit <cedric@vintedge.com>
2017-06-26 17:48:34 +0800

    Final commit :sunglasses:

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-9e7151d5-5a54-11e7-8f40-e8b1fc836bbb
+import React from 'react';
+import { render } from 'react-dom';
+
+class HelloWorldComponent extends React.Component {
+  render() {
+    return <h1>Hello from {this.props.phrase}!</h1>;
+  }
+}
+
+function init() {
+  var rootElement = document.createElement('div');
+  rootElement.className = 'root';
+  document.body.appendChild(rootElement);
+
+  render(
+    <HelloWorldComponent phrase='ES6' />,
+    rootElement
+  );
+}
+
+// Initialize
+init();
